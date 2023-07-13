33f158588cedfa6f3af3675475fc0dc3459262c2
zdm <zdm@softvisio.net>
2023-07-13 16:03:42 +0300

    fix: external resource index

--- bin/build.js
+++ bin/build.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import Cli from "#core/cli";
-import ExternalResourceBuilder from "#lib/external-resource-builder";
+import ExternalResourceBuilder from "#core/external-resource-builder";
 import Geolite2Asn from "#lib/external-resources/geolite2-asn";
 
 const CLI = {

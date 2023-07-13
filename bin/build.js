7a05ed400c7fb6182a9e0518f2fb6ff342ec9eb3
zdm <zdm@softvisio.net>
2023-07-13 16:03:06 +0300

    fix: external resource index

--- bin/build.js
+++ bin/build.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 import Cli from "#core/cli";
+import ExternalResourceBuilder from "#lib/external-resource-builder";
 import Geolite2Asn from "#lib/external-resources/geolite2-asn";
 
 const CLI = {
@@ -18,7 +19,13 @@ const CLI = {
 
 await Cli.parse( CLI );
 
-var res;
+const res = await ExternalResourceBuilder.build(
+    [
 
-res = await new Geolite2Asn().build( { "force": process.cli.options.force } );
+        //
+        Geolite2Asn,
+    ],
+    { "force": process.cli.options.force }
+);
+
 if ( !res.ok ) process.exit( 1 );
